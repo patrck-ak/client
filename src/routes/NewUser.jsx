f69d02baf3df12965faa018a63de8cff59d40ead
Patrick <patrickoliveira.silva20@gmail.com>
2023-12-16 16:21:30 -0300

    update dev cfg - 0.0.1

--- src/routes/NewUser.jsx
+++ src/routes/NewUser.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import Style from './css/NewUser.module.css'
+import 'bootstrap'
 
 function NewUser() {
+
+  const [userID, setUserID] = useState()
+  const [pass, setPass] = useState()
+  const [confirm, setConfirm] = useState()
+  const [level, setLevel] = useState()
+
   return (
     <>
-      <form>
-      <input type="text" name="id" id="id" placeholder="ID" required/>
-      <input type="password" name="pass" id="pass" placeholder="Senha" required/>
-      <input type="password" name="confirm" id="confirm" placeholder="Senha" required/>
-      <input type="number" name="level" id="level" />
+      <form className={Style.form}> 
+      <input type="text" name="id" onChange={(e) => setUserID(e.target.value)} id="id" placeholder="ID" required/>
+      <input type="password" name="pass" onChange={(e) => setPass(e.target.value)} id="pass" placeholder="Senha" required/>
+      <input type="password" name="confirm" onChange={(e) => setConfirm(e.target.value)} id="confirm" placeholder="Senha" required/>
+      <input type="number"  onChange={(e) => setLevel(e.target.value)} name="level" id="level" maxLength={1} />
 
+      <button type="submit">Cadastrar</button>
 
       </form>
     </>
